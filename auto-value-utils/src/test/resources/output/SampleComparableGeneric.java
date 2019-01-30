493ea245a57b262a36fd8eb1fd9e87e377ae91ba
denis-itskovich <denis.itskovich@dell.com>
2019-01-30 11:37:45 +0200

    [autovalue-utils] Removing redundant import

--- auto-value-utils/src/test/resources/output/SampleComparableGeneric.java
+++ auto-value-utils/src/test/resources/output/SampleComparableGeneric.java
@@ -1,6 +1,5 @@
 package com.slimgears.sample;
 
-import com.google.common.collect.ImmutableList;
 import com.slimgears.util.autovalue.annotations.PropertyMeta;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
