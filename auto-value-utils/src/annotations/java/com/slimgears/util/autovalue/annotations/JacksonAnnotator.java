1fa3ba5064e0be882a42873598fab1ccbba15930
denis-itskovich <denis.itskovich@dell.com>
2019-04-03 21:43:45 +0300

    [auto-value-utils] Adding JacksonAnnotator

---
+++ auto-value-utils/src/annotations/java/com/slimgears/util/autovalue/annotations/JacksonAnnotator.java
@@ -0,0 +1,12 @@
+package com.slimgears.util.autovalue.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@AutoValuePrototype.Annotator("com.slimgears.util.autovalue.apt.extensions.JacksonAnnotator")
+public @interface JacksonAnnotator {
+}
