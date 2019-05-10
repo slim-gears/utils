5e8eaf4ed362ac004cba3c00efbbbc51703df9c4
denis-itskovich <denis.itskovich@dell.com>
2019-05-10 22:48:58 +0300

    [auto-value-utils] Adding toBuilder() to HasMetaClass

--- auto-value-utils/src/annotations/java/com/slimgears/util/autovalue/annotations/HasMetaClass.java
+++ auto-value-utils/src/annotations/java/com/slimgears/util/autovalue/annotations/HasMetaClass.java
@@ -2,6 +2,7 @@ package com.slimgears.util.autovalue.annotations;
 
 public interface HasMetaClass<S> {
     MetaClass<S> metaClass();
+    MetaBuilder<S> toBuilder();
 
     interface Builder<_B extends Builder<_B, S>, S> {
         _B metaClass(MetaClass<S> metaClass);
