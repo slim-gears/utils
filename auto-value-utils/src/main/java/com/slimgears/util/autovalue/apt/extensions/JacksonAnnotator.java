91d53f7b5b5cc6e31ba67a653d121d7a31d9f05d
denis-itskovich <denis.itskovich@dell.com>
2021-03-22 11:21:05 +0200

    [auto-value-utils] Fixing jackson annotation

--- auto-value-utils/src/main/java/com/slimgears/util/autovalue/apt/extensions/JacksonAnnotator.java
+++ auto-value-utils/src/main/java/com/slimgears/util/autovalue/apt/extensions/JacksonAnnotator.java
@@ -1,23 +1,19 @@
 package com.slimgears.util.autovalue.apt.extensions;
 
 import com.google.auto.service.AutoService;
+import com.google.common.base.Strings;
 import com.slimgears.apt.data.AnnotationInfo;
 import com.slimgears.apt.data.AnnotationValueInfo;
 import com.slimgears.apt.data.TypeInfo;
 import com.slimgears.util.autovalue.annotations.UseJacksonAnnotator;
 import com.slimgears.util.autovalue.apt.Context;
 import com.slimgears.util.autovalue.apt.PropertyInfo;
-import com.slimgears.util.generic.MoreStrings;
 import com.slimgears.util.stream.Streams;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.ElementType;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-import static com.slimgears.util.generic.MoreStrings.lazy;
 import static java.util.Collections.singleton;
 
 @AutoService(Annotator.class)
@@ -25,23 +21,19 @@ import static java.util.Collections.singleton;
 public class JacksonAnnotator implements Annotator {
     @Override
     public Iterable<AnnotationInfo> annotatePropertyMethod(Context context, PropertyInfo property) {
-//        return singleton(AnnotationInfo.ofType(
-//                "com.fasterxml.jackson.annotation.JsonProperty",
-//                AnnotationValueInfo.ofPrimitive("value", property.name())));
-
-        List<AnnotationInfo> annotationInfos = property.annotations().stream()
+        TypeInfo jsonPropertyAnnotationType = TypeInfo.of("com.fasterxml.jackson.annotation.JsonProperty");
+        Map<TypeInfo, AnnotationInfo> annotationInfos = property.annotations().stream()
                 .filter(a -> a.type().packageName().startsWith("com.fasterxml.jackson.annotation") ||
                         a.type().simpleName().equals("Nullable") ||
                         a.type().simpleName().equals("NonNull"))
-                .collect(Collectors.toList());
+                .collect(Collectors.toMap(AnnotationInfo::type, a -> a));
 
-        TypeInfo jsonPropertyAnnotationType = TypeInfo.of("com.fasterxml.jackson.annotation.JsonProperty");
-        if (!property.hasAnnotation(jsonPropertyAnnotationType)) {
-            annotationInfos.add(AnnotationInfo.ofType(jsonPropertyAnnotationType,
+        if (!annotationInfos.containsKey(jsonPropertyAnnotationType) || Strings.isNullOrEmpty(String.valueOf(annotationInfos.get(jsonPropertyAnnotationType).getValue("value").primitive()))) {
+            annotationInfos.put(jsonPropertyAnnotationType, AnnotationInfo.ofType(jsonPropertyAnnotationType,
                     AnnotationValueInfo.ofPrimitive("value", property.name())));
         }
 
-        return annotationInfos;
+        return annotationInfos.values();
     }
 
     @Override
